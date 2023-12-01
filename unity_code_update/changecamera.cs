0fd9acfd6ce0bdfeaed33cb9d5eca50dfdc9787a
ChaceHH-H <146041784+ChaceHH-H@users.noreply.github.com>
2023-12-01 23:25:19 +0000

    Add files via upload
    Camera switching function completed, prefab generation code updated

---
+++ unity_code_update/changecamera.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class changecamera : MonoBehaviour
+{
+    public ARTrackedImageManager ARimage;
+    public ARFaceManager ARface;
+    private GameObject ARobject;
+    public ARCameraManager cameraManager{
+        get =>m_CameraManager;
+        set =>m_CameraManager=value;
+    }
+    [SerializeField]
+    ARCameraManager m_CameraManager;
+
+    void Start(){
+        //ARface=GetComponent<ARFaceManager>();
+        //ARimage=GetComponent<ARTrackedImageManager>();
+    }
+    
+
+    public void cameraswitch(){
+        Debug.Assert(m_CameraManager != null, "camera manager cannot be null");
+        CameraFacingDirection newfacingdirection;
+        switch(m_CameraManager.requestedFacingDirection){
+            case CameraFacingDirection.User:
+            newfacingdirection = CameraFacingDirection.World;
+            ARobject= GameObject.FindWithTag("ARface");
+            Destroy(ARobject);
+            ARface.enabled=false;
+            break;
+            case CameraFacingDirection.World:
+            default:
+            newfacingdirection  =CameraFacingDirection.User;
+            ARface.enabled=true;
+            break;
+        }
+        cameraManager.requestedFacingDirection=newfacingdirection;
+    }
+}
--- unity_code_update/servo.cs
+++ unity_code_update/servo.cs
@@ -14,18 +14,21 @@ using XCharts.Runtime;
 
 public class servo : MonoBehaviour
 {
-    private Button button;
+    //private Dropdown[] dropdowns= new Dropdown[6];
+    private TMP_Dropdown dropdown;
 
     private GameObject buttonactive;
     public GameObject face1;
     public GameObject face2;
-    public GameObject effect1;
+    public GameObject raineffect;
+    public GameObject snowffect;
+    public GameObject cloudsffect;
 
     public int servoPin = 13;
     [Range(-100, 180)]
     public int servoAngle = 0;
     private int prevServoAngle = 0;
-    public int citynum = 1;
+    public int citynum = 0;
     private double temp;
 
     public TextMeshPro Citytext;
@@ -36,9 +39,12 @@ public class servo : MonoBehaviour
     public TextMeshPro Weathetext;
 
     public GameObject pointer;
-    
     public float initialvalue = 0;
     private float xvalueD;
+    [Range(0, 180)]
+    public float testangle;
+    [Range(-20,20)]
+    public float testtime;
     private float tempangle;
 
     private string URL;
@@ -105,18 +111,18 @@ public class servo : MonoBehaviour
 
 
     void Start() {
-        buttonactive = findinactiveobjectName("Buttoncity");
-        buttonactive.gameObject.SetActive(true);
+        //buttonactive = findinactiveobjectName("Buttoncity");
+        //buttonactive.gameObject.SetActive(true);
+        dropdown = GameObject.Find("Dropdown").GetComponent<TMP_Dropdown>();
+        dropdown.onValueChanged.AddListener(delegate{
+          DropdownValueChanged(dropdown);
+        });
         UduinoManager.Instance.pinMode(servoPin , PinMode.Servo);
-        //Citytext = GameObject.Find("city");
-        //Temptext = GameObject.Find("temp");
-        button = GameObject.Find("Button").GetComponent<Button>();
-        button.onClick.AddListener(OnClick);
+
         xvalueD =(float)20.541;
-        cityname = "London";
-        //URL = "https://api.open-meteo.com/v1/forecast?latitude=51.541776123647395&longitude=-0.005828282697914817&current=temperature_2m&forecast_days=1";
-        URL="https://api.open-meteo.com/v1/forecast?latitude=51.541776123647395&longitude=-0.005828282697914817&current=temperature_2m,weather_code&daily=weather_code,temperature_2m_max,temperature_2m_min";
-        refresh();
+        //cityname = "London";
+        //URL="https://api.open-meteo.com/v1/forecast?latitude=51.541776123647395&longitude=-0.005828282697914817&current=temperature_2m,weather_code&daily=weather_code,temperature_2m_max,temperature_2m_min";
+        //refresh();
     }
 
     void Update()
@@ -126,59 +132,102 @@ public class servo : MonoBehaviour
             UduinoManager.Instance.analogWrite(servoPin, servoAngle);
             prevServoAngle = servoAngle;
         }
-    }
-
-    void OnClick(){
-		servochange();
-	  }
-
-    public void servochange(){
-        citynum ++;
-        if(citynum==6){
-            citynum = 1;
-        }
-        Debug.Log(citynum);
         
-          switch(citynum){
-            case 1:
-            servoAngle = 20;
-            URL="https://api.open-meteo.com/v1/forecast?latitude=51.541776123647395&longitude=-0.005828282697914817&current=temperature_2m,weather_code&daily=weather_code,temperature_2m_max,temperature_2m_min";
-            cityname = "London";
-            refresh();
-            Debug.Log(URL);
-            break;
-            case 2:
-            servoAngle = 40;
-            URL="https://api.open-meteo.com/v1/forecast?latitude=39.91002736793717&longitude=116.39694154457653&current=temperature_2m,weather_code&daily=weather_code,temperature_2m_max,temperature_2m_min";
-            cityname = "Beijing";
-            refresh();
-            Debug.Log(URL);
-            break;
-            case 3:
-            servoAngle = 60;
-            URL="https://api.open-meteo.com/v1/forecast?latitude=38.89697955298303&longitude=-77.03655378636529&current=temperature_2m,weather_code&daily=weather_code,temperature_2m_max,temperature_2m_min";
-            cityname = "Washington";
-            refresh();
-            Debug.Log(URL);
-            break;
-            case 4:
-            servoAngle = 80;
-            URL="https://api.open-meteo.com/v1/forecast?latitude=55.75320382676515&longitude=37.62040600688334&current=temperature_2m,weather_code&daily=weather_code,temperature_2m_max,temperature_2m_min";
-            cityname = "Moscow";
-            refresh();
-            Debug.Log(URL);
-            break;
-            case 5:
-            servoAngle = 100;
-            URL="https://api.open-meteo.com/v1/forecast?latitude=35.682093616671004&longitude=139.7668859932433&current=temperature_2m,weather_code&daily=weather_code,temperature_2m_max,temperature_2m_min";
-            cityname = "Tokyo";
-            refresh();
-            Debug.Log(URL);
-            break;
-            default:
-            break;
-        }
+        //pointer.transform.Rotate(new Vector3(0f,testangle,0f),Space.Self);
+
     }
+
+
+
+    void DropdownValueChanged(TMP_Dropdown change){
+      Debug.Log(change.value);
+      switch(change.value){
+        case 1:
+        URL="https://api.open-meteo.com/v1/forecast?latitude=51.541776123647395&longitude=-0.005828282697914817&current=temperature_2m,weather_code&daily=weather_code,temperature_2m_max,temperature_2m_min";
+        cityname = "London";
+        citynum=1;
+        refresh();
+        break;
+        case 2:
+        URL="https://api.open-meteo.com/v1/forecast?latitude=39.91002736793717&longitude=116.39694154457653&current=temperature_2m,weather_code&daily=weather_code,temperature_2m_max,temperature_2m_min";
+        cityname = "Beijing";
+        citynum=2;
+        refresh();
+        break;
+        case 3:
+        URL="https://api.open-meteo.com/v1/forecast?latitude=38.89697955298303&longitude=-77.03655378636529&current=temperature_2m,weather_code&daily=weather_code,temperature_2m_max,temperature_2m_min";
+        cityname = "Washington";
+        citynum=3;
+        refresh();
+        break;
+        case 4:
+        URL="https://api.open-meteo.com/v1/forecast?latitude=55.75320382676515&longitude=37.62040600688334&current=temperature_2m,weather_code&daily=weather_code,temperature_2m_max,temperature_2m_min";
+        cityname = "Moscow";
+        citynum=4;
+        refresh();
+        break;
+        case 5:
+        URL="https://api.open-meteo.com/v1/forecast?latitude=35.682093616671004&longitude=139.7668859932433&current=temperature_2m,weather_code&daily=weather_code,temperature_2m_max,temperature_2m_min";
+        cityname = "Tokyo";
+        citynum=5;
+        refresh();
+        break;
+        default:
+        break;
+
+      }
+    }
+
+    
+
+    // public void servochange(){
+      
+    //     citynum ++;
+    //     if(citynum==6){
+    //         citynum = 1;
+    //     }
+    //     Debug.Log(citynum);
+        
+          // switch(citynum){
+          //   case 1:
+          //   servoAngle = 20;
+          //   URL="https://api.open-meteo.com/v1/forecast?latitude=51.541776123647395&longitude=-0.005828282697914817&current=temperature_2m,weather_code&daily=weather_code,temperature_2m_max,temperature_2m_min";
+          //   cityname = "London";
+          //   refresh();
+          //   Debug.Log(URL);
+          //   break;
+          //   case 2:
+          //   servoAngle = 40;
+          //   URL="https://api.open-meteo.com/v1/forecast?latitude=39.91002736793717&longitude=116.39694154457653&current=temperature_2m,weather_code&daily=weather_code,temperature_2m_max,temperature_2m_min";
+          //   cityname = "Beijing";
+          //   refresh();
+          //   Debug.Log(URL);
+          //   break;
+          //   case 3:
+          //   servoAngle = 60;
+          //   URL="https://api.open-meteo.com/v1/forecast?latitude=38.89697955298303&longitude=-77.03655378636529&current=temperature_2m,weather_code&daily=weather_code,temperature_2m_max,temperature_2m_min";
+          //   cityname = "Washington";
+          //   refresh();
+          //   Debug.Log(URL);
+          //   break;
+          //   case 4:
+          //   servoAngle = 80;
+          //   URL="https://api.open-meteo.com/v1/forecast?latitude=55.75320382676515&longitude=37.62040600688334&current=temperature_2m,weather_code&daily=weather_code,temperature_2m_max,temperature_2m_min";
+          //   cityname = "Moscow";
+          //   refresh();
+          //   Debug.Log(URL);
+          //   break;
+          //   case 5:
+          //   servoAngle = 100;
+          //   URL="https://api.open-meteo.com/v1/forecast?latitude=35.682093616671004&longitude=139.7668859932433&current=temperature_2m,weather_code&daily=weather_code,temperature_2m_max,temperature_2m_min";
+          //   cityname = "Tokyo";
+          //   refresh();
+          //   Debug.Log(URL);
+          //   break;
+          //   default:
+          //   break;
+          // }
+    // }
 
   private IEnumerator GetDatas(string Url,string cityname){
     using(UnityWebRequest webRequest = UnityWebRequest.Get(Url)){
@@ -186,14 +235,6 @@ public class servo : MonoBehaviour
       if(webRequest.result == UnityWebRequest.Result.ConnectionError){
         Debug.LogError(webRequest.error);
       }else{
-        //Root root= JsonMapper.ToObject<Root>(webRequest.downloadHandler.text);
-        //double temp = root.current.temperature_2m;
-        //Citytext.text = cityname;
-        //Temptext.text = temp.ToString();
-        //tempangle = -(0 +(float)temp);
-        //pointer.transform.localRotation = Quaternion.Euler(0,tempangle,0);
-        
-
         var text =webRequest.downloadHandler.text;
         Root root = JsonConvert.DeserializeObject<Root>(text);
         //Debug.Log(text);
@@ -203,7 +244,7 @@ public class servo : MonoBehaviour
         List<double> dailymaxtemp = root.daily.temperature_2m_max;
         List<double> dailymintemp = root.daily.temperature_2m_min;
         List<string> time = root.daily.time;
-        Debug.Log(dailymaxtemp);
+        //Debug.Log(dailymaxtemp);
         int weather_code = root.current.weather_code;
         weathertype(weather_code);
         Citytext.text = cityname;
@@ -211,8 +252,8 @@ public class servo : MonoBehaviour
         //tempangle = -(initialvalue +(float)temp);
         tempangle = initialvalue +(float)temp;
         pointer.transform.localRotation = Quaternion.Euler(xvalueD,-180,0);
-        pointer.transform.Rotate(new Vector3(0f,tempangle,0f),Space.Self);
-
+        pointer.transform.Rotate(new Vector3(0f,tempangle,0f),1f,Space.Self);
+        UduinoManager.Instance.sendCommand("tempdata", temp,citynum);
 
         //lineChart.RemoveData();
         lineChart.ClearData();
@@ -233,20 +274,24 @@ public class servo : MonoBehaviour
   }
 
   void weathertype(int Wcode){
-    effect1.SetActive(false);
+    raineffect.SetActive(false);
+    snowffect.SetActive(false);
+    cloudsffect.SetActive(false);
     if(Wcode==0){
       weather = "Clear sky";
     }else if(Wcode==1||Wcode==2||Wcode==3){
       weather="Partly cloudy";
+      cloudsffect.SetActive(true);
     }else if(Wcode==45||Wcode==48){
       weather="Fog";
     }else if(Wcode==51||Wcode==53||Wcode==55||Wcode==56||Wcode==57){
       weather="Drizzle";
     }else if(Wcode==61||Wcode==63||Wcode==65||Wcode==66||Wcode==67||Wcode==80||Wcode==81||Wcode==82){
       weather="Rain";
-      effect1.SetActive(true);
+      raineffect.SetActive(true);
     }else if(Wcode==71||Wcode==73||Wcode==75||Wcode==77||Wcode==85||Wcode==86){
       weather="Snow";
+      snowffect.SetActive(true);
     }else if(Wcode==95){
       weather="Thunderstorm";
     }else if(Wcode==96||Wcode==99){
