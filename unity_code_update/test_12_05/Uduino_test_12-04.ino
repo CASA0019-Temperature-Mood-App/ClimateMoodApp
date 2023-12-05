0f8dc8639cfcfd99306f9c11950afd905da6f11f
ChaceHH-H <146041784+ChaceHH-H@users.noreply.github.com>
2023-12-05 17:00:15 +0000

    updata prefab，add button to change position

---
+++ unity_code_update/test_12_05/Uduino_test_12-04.ino
@@ -0,0 +1,96 @@
+// Uduino settings
+#include <Uduino_Wifi.h>
+Uduino_Wifi uduino("uduinoBoard"); // Declare and name your object
+#include <U8g2lib.h> // 引入U8g2库
+#include <Servo.h>
+#include <DHT.h>
+
+#define DHTPIN 2  // DHT22数据引脚为GPIO2
+#define DHTTYPE DHT22
+
+DHT dht(DHTPIN, DHTTYPE);
+U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+Servo myservo;
+float valueOne;
+int valueTwo;
+
+void setup()
+{
+  Serial.begin(9600);
+  uduino.addCommand("tempdata", getdata);
+  dht.begin();
+  u8g2.begin();
+  u8g2.setFont(u8g2_font_ncenB08_tr);
+  myservo.attach(0);
+  
+
+  // Optional function,  to add BEFORE connectWifi(...)
+  uduino.setPort(4222);   // default 4222
+
+  uduino.useSendBuffer(true); // default true
+  uduino.setConnectionTries(35); // default 35
+  uduino.useSerial(true); // default is true
+
+  // mendatory function
+  uduino.connectWifi("CE-Hub-Student", "casa-ce-gagarin-public-service");
+  //uduino.connectWifi("mate40pro", "88888888");
+  //uduino.connectWifi("Chacewifi", "88888888");
+}
+
+void loop()
+{
+  uduino.update();
+
+  if (uduino.isConnected()) {
+    float temperature = dht.readTemperature();
+    uduino.println(temperature);
+    uduino.delay(5000);
+  }else{
+    float temperature = dht.readTemperature();
+    String tempStr = String(temperature) + " C";
+    u8g2.clearBuffer();
+    u8g2.drawStr(50, 20, "Local");
+    u8g2.drawStr(50, 40, tempStr.c_str());
+    u8g2.sendBuffer();
+    int val = map(temperature, 45, -15, 0, 225);
+    myservo.write(val);
+    uduino.println(0);
+    uduino.delay(5000);
+  }
+}
+
+void getdata(){
+  int parameters = uduino.getNumberOfParameters(); 
+  if(parameters > 0) {
+  valueOne = uduino.charToInt(uduino.getParameter(0)); 
+  valueTwo = uduino.charToInt(uduino.getParameter(1)); 
+  }
+  u8g2.clearBuffer();
+  String tempStr = String(valueOne) + " C";
+    switch(valueTwo){
+    case 0:
+    u8g2.drawStr(50, 20, "Local");
+    break;
+    case 1:
+    u8g2.drawStr(50, 20, "London");
+    break;
+    case 2:
+    u8g2.drawStr(50, 20, "Beijing");
+    break;
+    case 3:
+    u8g2.drawStr(50, 20, "Washington");
+    break;
+    case 4:
+    u8g2.drawStr(50, 20, "Moscow");
+    break;
+    case 5:
+    u8g2.drawStr(50, 20, "Tokyo");
+    break;
+    default:
+    break;
+  }
+  u8g2.drawStr(50, 40, tempStr.c_str());  // 写入字符串
+  u8g2.sendBuffer();
+  int val = map(valueOne, 45, -15, 0, 225);
+  myservo.write(val);
+}
